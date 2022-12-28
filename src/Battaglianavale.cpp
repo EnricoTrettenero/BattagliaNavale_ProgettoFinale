087ee71bcea5026a2e725b0a6fd59a7db2107a25
Enry234 <enrico.trettenero@studenti.unipd.it>
2022-12-28 11:14:24 +0100

    chore: start implements defense

--- src/Battaglianavale.cpp
+++ src/Battaglianavale.cpp
@@ -4,10 +4,20 @@
 #include <iostream>
 #include "../lib/ship/Battleship.h"
 #include "../lib/board/Attack.h"
+#include "../lib/board/Defense.h"
+#include "../lib/ship/Support.h"
+#include "../lib/ship/Submarine.h"
 using namespace std;
 
 int main()
 {
-
+    defense d = defense();
+    battleship s = battleship(battleships::coordinate(10,'B'),ship::VERTICAL);
+    cout<<d.setShip(s );
+    support ss = support(battleships::coordinate(10,'F'),ship::HORIZONTAL);
+    cout<<d.setShip(ss);
+    submarine sm = submarine(battleships::coordinate(12,'G'),ship::HORIZONTAL);
+    cout<<d.setShip(sm);
+    cout<<endl<<d<<endl;
   return 0;
 }
--- src/infra/board/Defense.cpp
+++ src/infra/board/Defense.cpp
@@ -9,11 +9,35 @@ defense::defense()
     {
         for (int j = 0; j < kDimBoard; ++j)
         {
-            _matrix[i][j]= ' ';
+            _matrix[i][j] = '*';
         }
     }
 }
 bool defense::setShip(ship &s)
 {
+    //check space on board
+    for (int i = 0; i < s.dim(); ++i)
+    {
+        if (s.getOrientation() == ship::HORIZONTAL)
+        {
+            if ((s.center().y() + i - s.dim() / 2)<0||s.center().y() + i - s.dim() / 2>11||_matrix[s.center().x() ][s.center().y()+ i - s.dim() / 2] != '*') //assume all ship is odd
 
+                return false;
+        } else
+        {
+            if ((s.center().x() + i - s.dim() / 2)<0||s.center().x() + i - s.dim() / 2>11||_matrix[s.center().x() + i - s.dim() / 2][s.center().y()] != '*') //assume all ship is odd
+                return false;
+        }
+    }
+    for (int i = 0; i < s.dim(); ++i)
+    {
+        if (s.getOrientation() == ship::HORIZONTAL)
+        {
+            _matrix[s.center().x() ][s.center().y()+ i - s.dim() / 2] = s.armor()[i];//assume all ship is odd
+        } else
+        {
+            _matrix[s.center().x() + i - s.dim() / 2][s.center().y()] = s.armor()[i];
+        }
+    }
+    return true;
 }
